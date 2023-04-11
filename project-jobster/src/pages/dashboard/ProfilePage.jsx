9ca9da9305cdd6493769ae2e679b192d3877802c
김영훈 <joseph0926@naver.com>
2023-04-11 19:30:10 +0900

    2023.04.11

--- project-jobster/src/pages/dashboard/ProfilePage.jsx
+++ project-jobster/src/pages/dashboard/ProfilePage.jsx
@@ -1,7 +1,121 @@
-import React from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import InputHelper from "../../components/InputHelper";
+
+import styled from "styled-components";
+import { updateUser } from "../../store/user/user-slice";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const dispatchFn = useDispatch();
+  const { isLoading, user } = useSelector((state) => state.user);
+
+  const [userData, setUserData] = useState({
+    name: user?.name || "",
+    email: user?.email || "",
+    lastName: user?.lastName || "",
+    location: user?.location || "",
+  });
+
+  const inputHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const { name, email, lastName, location } = userData;
+    if (!name || !email || !lastName || !location) {
+      toast.error("빈 항목이 존재해서는 안됩니다.");
+      return;
+    }
+    dispatchFn(updateUser({ name, email, lastName, location }));
+  };
+
+  return (
+    <Wrapper>
+      <form className="form" onSubmit={submitHandler}>
+        <h3>profile</h3>
+
+        <div className="form-center">
+          <InputHelper type="text" name="name" value={userData.name} handleChange={inputHandler} />
+          <InputHelper type="text" labelText="last name" name="lastName" value={userData.lastName} handleChange={inputHandler} />
+          <InputHelper type="email" name="email" value={userData.email} handleChange={inputHandler} />
+          <InputHelper type="text" name="location" value={userData.location} handleChange={inputHandler} />
+          <button className="btn btn-block" type="submit" disabled={isLoading}>
+            {isLoading ? "Please Wait..." : "save changes"}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  border-radius: var(--borderRadius);
+  width: 100%;
+  background: var(--white);
+  padding: 3rem 2rem 4rem;
+  box-shadow: var(--shadow-2);
+  h3 {
+    margin-top: 0;
+  }
+  .form {
+    margin: 0;
+    border-radius: 0;
+    box-shadow: none;
+    padding: 0;
+    max-width: 100%;
+    width: 100%;
+  }
+  .form-row {
+    margin-bottom: 0;
+  }
+  .form-center {
+    display: grid;
+    row-gap: 0.5rem;
+  }
+  .form-center button {
+    align-self: end;
+    height: 35px;
+    margin-top: 1rem;
+  }
+  .btn-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 1rem;
+    align-self: flex-end;
+    margin-top: 0.5rem;
+    button {
+      height: 35px;
+    }
+  }
+  .clear-btn {
+    background: var(--grey-500);
+  }
+  .clear-btn:hover {
+    background: var(--black);
+  }
+  @media (min-width: 992px) {
+    .form-center {
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+      column-gap: 1rem;
+    }
+    .btn-container {
+      margin-top: 0;
+    }
+  }
+  @media (min-width: 1120px) {
+    .form-center {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+    .form-center button {
+      margin-top: 0;
+    }
+  }
+`;
 
 export default ProfilePage;
--- project-jobster/src/store/user/user-slice.jsx
+++ project-jobster/src/store/user/user-slice.jsx
@@ -1,10 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
-import {
-  getUserFromLocalStorage,
-  addUserToLocalStorage,
-  removeUserFromLocalStorage,
-} from "../../utils/localStorage";
+import { getUserFromLocalStorage, addUserToLocalStorage, removeUserFromLocalStorage } from "../../utils/localStorage";
 import { redirect } from "react-router-dom";
 
 const url = "https://jobify-prod.herokuapp.com/api/v1/toolkit";
@@ -15,29 +11,26 @@ const initialState = {
   user: getUserFromLocalStorage(),
 };
 
-export const signup = createAsyncThunk(
-  "user/signup",
-  async (user, thunkAPI) => {
-    console.log(user);
-    try {
-      const response = await fetch(`${url}/auth/register`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-      });
-      const data = await response.json();
+export const signup = createAsyncThunk("user/signup", async (user, thunkAPI) => {
+  console.log(user);
+  try {
+    const response = await fetch(`${url}/auth/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    const data = await response.json();
 
-      if (!response.ok) {
-        throw data;
-      }
-      return data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.msg);
+    if (!response.ok) {
+      throw data;
     }
+    return data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.msg);
   }
-);
+});
 export const login = createAsyncThunk("user/login", async (user, thunkAPI) => {
   try {
     const response = await fetch(`${url}/auth/login`, {
@@ -58,6 +51,32 @@ export const login = createAsyncThunk("user/login", async (user, thunkAPI) => {
   }
 });
 
+export const updateUser = createAsyncThunk("user/updateUser", async (user, thunkAPI) => {
+  try {
+    const response = await fetch(`${url}/auth/updateUser`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+      },
+      body: JSON.stringify(user),
+    });
+    const data = await response.json();
+
+    if (response.status === 401) {
+      thunkAPI.dispatch(logout());
+      return thunkAPI.rejectWithValue("인증오류가 발생하였습니다,,,");
+    }
+    if (!response.ok) {
+      throw data;
+    }
+
+    return data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.msg);
+  }
+});
+
 const userSlice = createSlice({
   name: "user",
   initialState,
@@ -68,6 +87,7 @@ const userSlice = createSlice({
     logout: (state) => {
       state.user = null;
       state.isSidebarOpen = false;
+      toast.success("로그아웃에 성공하셨습니다");
       removeUserFromLocalStorage();
     },
   },
@@ -101,6 +121,21 @@ const userSlice = createSlice({
         toast.success(`안녕하세요! ${user.name}님`);
       })
       .addCase(login.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      })
+      .addCase(updateUser.pending, (state, { payload }) => {
+        state.isLoading = true;
+      })
+      .addCase(updateUser.fulfilled, (state, { payload }) => {
+        const { user } = payload;
+        state.isLoading = true;
+        state.user = user;
+
+        addUserToLocalStorage(user);
+        toast.success("사용자 정보 업데이트 성공");
+      })
+      .addCase(updateUser.rejected, (state, { payload }) => {
         state.isLoading = false;
         toast.error(payload);
       });
