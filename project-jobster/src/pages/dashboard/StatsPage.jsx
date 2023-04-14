5fecbb158f4bdf9e120c961b8118eaa8a4fae448
김영훈 <joseph0926@naver.com>
2023-04-14 18:05:15 +0900

    2023.04.14

--- project-jobster/src/pages/dashboard/StatsPage.jsx
+++ project-jobster/src/pages/dashboard/StatsPage.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { showStats } from "../../store/allJobs/allJobs-slice";
+import Loading from "../../components/Loading";
+import StatsContainer from "../../components/StatsContainer";
+import ChartsContainer from "../../components/ChartsContainer";
 
 const StatsPage = () => {
-  return <div>StatsPage</div>;
+  const dispatchFn = useDispatch();
+  const { isLoading, monthlyApplications } = useSelector((state) => state.allJobs);
+  useEffect(() => {
+    dispatchFn(showStats());
+  }, []);
+  if (isLoading) {
+    return <Loading center />;
+  }
+  return (
+    <>
+      <StatsContainer />
+      {monthlyApplications.length > 0 && <ChartsContainer />}
+    </>
+  );
 };
 
 export default StatsPage;
--- project-jobster/src/store/allJobs/allJobs-slice.jsx
+++ project-jobster/src/store/allJobs/allJobs-slice.jsx
@@ -41,6 +41,25 @@ export const getAllJobs = createAsyncThunk("allJobs/getAllJobs", async (_, thunk
   }
 });
 
+export const showStats = createAsyncThunk("allJobs/showStats", async (_, thunkAPI) => {
+  try {
+    const response = await fetch(`${url}/jobs/stats`, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+      },
+    });
+    if (!response.ok) {
+      throw await response.json();
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error);
+  }
+});
+
 const allJobsSlice = createSlice({
   name: "allJobs",
   initialState,
@@ -59,11 +78,21 @@ const allJobsSlice = createSlice({
       })
       .addCase(getAllJobs.fulfilled, (state, { payload }) => {
         state.isLoading = false;
-        console.log(payload.jobs);
         state.jobs = payload.jobs;
       })
       .addCase(getAllJobs.rejected, (state, { payload }) => {
-        console.log("asdf");
+        state.isLoading = false;
+        toast.error(payload);
+      })
+      .addCase(showStats.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(showStats.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.stats = payload.defaultStats;
+        state.monthlyApplications = payload.monthlyApplications;
+      })
+      .addCase(showStats.rejected, (state, { payload }) => {
         state.isLoading = false;
         toast.error(payload);
       });
